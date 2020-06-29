abe725d92c4e2b0861e91775ca782afa91a48fd8
f0rc1ng <forcenig_91@mail.ru>
2020-06-29 13:21:02 +0300

    Личный проект: больше деталей (часть 2)
    Первая попытка

--- js/main.js
+++ js/main.js
@@ -2,13 +2,13 @@
 
 // Функция для создания массива из 8 сгенерированных JS-объектов
 var ADS_QUANTITY = 8;
-var TITLES = ['Дворец', 'Квартира', 'Дом', 'Бунгало'];
+var TITLES = ['Идеальный вариант на выходные', 'Лучшее для семьи', 'Для большой компании'];
 var PIN_X_MIN = 0;
 var PIN_X_MAX = 1200;
 var PIN_Y_MIN = 130;
 var PIN_Y_MAX = 630;
 var MIN_PRICE = 0;
-var MAX_PRICE = 1000000;
+var MAX_PRICE = 100000;
 var TYPES = ['palace', 'flat', 'house', 'bungalo'];
 var ROOMS = [1, 2, 3];
 var GUESTS = [1, 2, 3];
@@ -49,8 +49,9 @@ var getArrRandomLength = function (arr) {
   return result;
 };
 
+var randomArr = [];
+
 var getData = function () {
-  var randomArr = [];
 
   for (var i = 1; i <= ADS_QUANTITY; i++) {
     var ad = {
@@ -120,3 +121,65 @@ renderPins();
 
 var map = document.querySelector('.map');
 map.classList.remove('map--faded');
+
+
+var typesoOffers = {
+  palace: 'Дворец',
+  flat: 'Квартира',
+  house: 'Дом',
+  bungalo: 'Бунгало'
+};
+
+var filtersContainer = document.querySelector('.map__filters-container');
+
+
+// функция склонения
+var getNoun = function (number, one, two, five) {
+  number = Math.abs(number);
+  number %= 100;
+  if (number >= 5 && number <= 20) {
+    return number + five;
+  }
+  number %= 10;
+  if (number === 1) {
+    return number + one;
+  }
+  if (number >= 2 && number <= 4) {
+    return number + two;
+  }
+  return number + five;
+};
+
+
+// Функция карточки объявления
+var getCard = function (value) {
+  var cardTemplate = document.querySelector('#card').content.querySelector('.map__card');
+  var cardElement = cardTemplate.cloneNode(true);
+
+  cardElement.querySelector('.popup__title').textContent = value.offer.title;
+  cardElement.querySelector('.popup__text--address').textContent = value.offer.address;
+  cardElement.querySelector('.popup__text--price').textContent = value.offer.price + ' ₽/ночь';
+  cardElement.querySelector('.popup__type').textContent = typesoOffers[value.offer.type];
+  cardElement.querySelector('.popup__text--capacity').textContent = getNoun(value.offer.rooms, ' комната для ', ' комнаты для ', ' комнат для ') + getNoun(value.offer.guests, ' гостя', ' гостей', ' гостей');
+  cardElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + value.offer.checkin + ', выезд до ' + value.offer.checkout;
+  cardElement.querySelector('.popup__description').textContent = value.offer.description;
+  cardElement.querySelector('.popup__avatar').src = value.author.avatar;
+  cardElement.querySelector('.popup__features').li = value.offer.features;
+
+  var popupPhotos = cardElement.querySelector('.popup__photos');
+  popupPhotos.textContent = '';
+  for (var j = 0; j < value.offer.photos.length; j += 1) {
+    var newPhoto = document.createElement('img');
+    newPhoto.classList.add('popup__photo');
+    newPhoto.setAttribute('src', value.offer.photos[j]);
+    newPhoto.setAttribute('width', 40);
+    newPhoto.setAttribute('height', 45);
+    newPhoto.setAttribute('alt', 'Фотография жилья');
+
+    popupPhotos.appendChild(newPhoto);
+  }
+
+  return cardElement;
+};
+
+map.insertBefore(getCard(randomArr[0]), filtersContainer);
